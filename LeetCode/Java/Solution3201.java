e94310a6d383b7d828f91d6cde75c69a49d4465e
GrahamPilk <graham.a.pilkington@Gmail.com>
2025-07-16 11:41:10 -0400

    3201

---
+++ LeetCode/Java/Solution3201.java
@@ -0,0 +1,30 @@
+package LeetCode.Java;
+
+public class Solution3201 {
+    public int maximumLength(int[] nums) {
+        int odd = 0;
+        int even = 0;
+        int count = 1;
+        int prev = 0;
+
+        if(nums[0] % 2 == 0) prev = 1;
+
+        for(int i  = 0; i < nums.length; i++) {
+            if(nums[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+
+            if(prev == 1 && nums[i] % 2 == 1) {
+                prev = 0;
+                count++;
+            } else if(prev == 0 && nums[i] % 2 == 0) {
+                count++;
+                prev = 1;
+            }
+
+        }
+        return Math.max(odd, Math.max(even, count));
+    }
+}
