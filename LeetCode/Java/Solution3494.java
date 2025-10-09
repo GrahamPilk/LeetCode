844c9033cf933d9e4fdfe5f98d16f945d9d70d63
GrahamPilk <graham.a.pilkington@gmail.com>
2025-10-08 22:20:28 -0400

    potions

---
+++ LeetCode/Java/Solution3494.java
@@ -0,0 +1,24 @@
+public class Solution3494 {
+    public long minTime(int[] skill, int[] mana) {
+        int n = skill.length, m = mana.length;
+        long[] done = new long[n + 1];
+        
+        for (int j = 0; j < m; ++j) {
+            for (int i = 0; i < n; ++i) {
+                done[i + 1] = Math.max(done[i + 1], done[i]) + (long) mana[j] * skill[i];
+            }
+            for (int i = n - 1; i > 0; --i) {
+                done[i] = done[i + 1] - (long) mana[j] * skill[i];
+            }
+        }
+        
+        return done[n];
+    }
+}
+
+/*
+ * You are given two integer arrays, skill and mana, of length n and m
+ * In a lab, n wizards must brew m potions in order. Each potion has a mana capacity mana[j] and must pass through all the wizards sequentially to be brewed properly. The time taken bvy the ith wizard on the jth ption is time ij = skill[i] * mana[j]
+ * Since the brewing process is delicate, a potion must be passed to the next wizard immediately after the current wizard completes their work. This meant the timing must be synchronzied so that each wizard begins working on a potion exactly when it arrives. 
+ * Return the minimum amount of time required for the potions to be brewed properly. 
+ */
