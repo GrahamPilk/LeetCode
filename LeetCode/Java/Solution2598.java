24b497f3b27ee58759e7a4d33760d99748bdfa5c
GrahamPilk <graham.a.pilkington@Gmail.com>
2025-10-16 09:38:19 -0400

    smallestInteger

---
+++ LeetCode/Java/Solution2598.java
@@ -0,0 +1,15 @@
+public class Solution2598 {
+    public int findSmallestInteger(int[] nums, int value) {
+        int[] freq = new int[value];
+        for(int num : nums) {
+            int r = num % value;
+            freq[r < 0 ? r + value : r]++;
+        }
+        for(int k = 0; ; k++){
+            int r = k % value;
+            if (freq[r]-- == 0) {
+                return k;
+            }
+        }
+    }
+}
