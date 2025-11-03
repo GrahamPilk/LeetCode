156f3eafe8fc9b4b41d8e46ed5e2ac534d71967d
GrahamPilk <graham.a.pilkington@gmail.com>
2025-11-03 12:03:31 -0500

    balloons time

---
+++ LeetCode/Java/Solution1578.java
@@ -0,0 +1,12 @@
+public class Solution1578 {
+    public int minCost(String colors, int[] neededTime) {
+        int min = 0;
+        for(int i = 1; i < colors.length(); i++) {
+            if(colors.charAt(i) == colors.charAt(i-1)) {
+                min += Math.min(neededTime[i], neededTime[i-1]);
+                neededTime[i] = Math.max(neededTime[i], neededTime[i-1]);
+            }
+        }
+        return min;
+    }
+}
